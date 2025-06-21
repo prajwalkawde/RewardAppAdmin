9173026f181c6f61aba4dc596a9d969efa6be896
prajwalkawde <126794081+prajwalkawde@users.noreply.github.com>
2025-06-21 20:44:10 +0530

    diamond section added in add amount page

--- resources/views/admin/reward/edit_rm_amounts.blade.php
+++ resources/views/admin/reward/edit_rm_amounts.blade.php
@@ -75,6 +75,18 @@
                                   </span>
                                 </span>
                               </label>
+                               <label class="block">
+                                <span>Diamond</span>
+                                <span class="relative mt-1.5 flex">
+                                  <input
+                                  name="diamond"
+                                  class="form-input peer w-full rounded-lg border border-slate-300 bg-transparent px-3 py-2 pl-9 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"
+                                  placeholder="©" type="number">
+                                  <span class="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
+                                      <i class="fa-solid fa-trophy"></i>
+                                  </span>
+                                </span>
+                              </label>
 
                             </div>
                       </label>
